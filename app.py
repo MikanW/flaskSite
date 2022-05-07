b5a949fa8ab4b79095a7f253d8964285f2d8c86f
mikan <zuxian.wan@gmail.com>
2022-05-07 17:11:04 +0800

    cookBook updated

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return '<h1>烤鞋底子都好吃系列食谱（缓慢施工中）</h1><div><ul><li><a href="static/cookBook/swtPttCrnTrt.html">红薯玉米挞</a></li></ul></div></footer>'
